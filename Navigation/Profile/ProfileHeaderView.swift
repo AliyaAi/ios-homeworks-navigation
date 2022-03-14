bdacbb3ccf575e219862e9d612989f6cfd794514
AliyaAi <aliyaaituganova@gmail.com>
2022-03-15 02:41:21 +0500

    1

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Александр Якубов on 15.03.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
